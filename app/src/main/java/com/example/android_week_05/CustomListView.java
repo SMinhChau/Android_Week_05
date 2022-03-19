c949d7c1027159abbf3415b281913b47e25d745f
minhchau <minhchaunguyenmtp@gmail.com>
2022-03-19 12:08:49 +0700

    update search + click button

--- app/src/main/java/com/example/android_week_05/CustomListView.java
+++ app/src/main/java/com/example/android_week_05/CustomListView.java
@@ -27,10 +27,9 @@ public class CustomListView extends AppCompatActivity {
     FoodAdapters adt;
     ArrayList<Food> arrayList;
     Button btnDount, btnPink,btnfloat;
-    private List<Food> cakeList;
-    private List<Food> list;
-    private List<Food> cakeBtn;
+
     private EditText textSearch;
+
 
     @Override
     public void onCreate( Bundle savedInstanceState) {
@@ -41,10 +40,10 @@ public class CustomListView extends AppCompatActivity {
         textSearch= findViewById(R.id.editTextName);
         btnDount = findViewById(R.id.btnDount);
         btnPink = findViewById(R.id.btnPink);
-        btnfloat=findViewById(R.id.btnAdd);
+        btnfloat=findViewById(R.id.btnFloat);
 
         arrayList = new ArrayList<>();
-        list= new ArrayList<>();
+
 
         arrayList.add(new Food("Tasty Donut","$10.00",R.drawable.donut_yellow_1));
         arrayList.add(new Food( "Pink Donut","$20.00",R.drawable.pink_donut_1));
@@ -55,9 +54,6 @@ public class CustomListView extends AppCompatActivity {
         adt= new FoodAdapters(this,R.layout.item_list,arrayList);
         idListView.setAdapter(adt);
 
-        cakeBtn = new ArrayList<>();
-        cakeList= new ArrayList<>();
-
         textSearch.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
@@ -66,40 +62,33 @@ public class CustomListView extends AppCompatActivity {
 
             @Override
             public void onTextChanged(CharSequence s, int i, int i1, int i2) {
-                cakeList.clear();
-
-                String regex ="^("+s.toString().toUpperCase()+").*";
-                for (Food cake: cakeBtn){
-                    if (cake.getTitle().toUpperCase().matches(regex)){
-                        cakeList.add(cake);
-                    }
-                }
-
-                adt.notifyDataSetChanged();
             }
 
             @Override
             public void afterTextChanged(Editable editable) {
-
+                adt.setFilterBySearch(editable.toString());
             }
         });
 
-        idListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+        btnDount.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-//                Food cale= list.get(i);
-//                Toast.makeText(getApplicationContext(),"dddd",Toast.LENGTH_LONG).show();
-//                Intent intent= new Intent(CustomListView.this,ActivityDonut.class);
-//
-//                Bundle bundle= new Bundle();
-//                bundle.putString("title",arrayList.get(i).getTitle());
-//                bundle.putString("price",arrayList.get(i).getRice());
-//                bundle.putInt("img",arrayList.get(i).getImg());
-//                intent.putExtra("cake",bundle);
-//                startActivity(intent);
+            public void onClick(View view) {
+                adt.setAllDonut();
             }
         });
-
+        btnPink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                adt.setFilterPink();
+            }
+        });
+        btnfloat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                adt.setFilterFloating();
+            }
+        });
     }
 
 
--- app/src/main/java/com/example/android_week_05/FoodAdapters.java
+++ app/src/main/java/com/example/android_week_05/FoodAdapters.java
@@ -18,17 +18,19 @@ public class FoodAdapters extends BaseAdapter {
     Context context;
     int layout;
     ArrayList<Food> arrayList;
+    ArrayList<Food> arrayFilter;
     private int positionSelect = -1;
 
     public FoodAdapters(Context context, int layout, ArrayList<Food> arrayList) {
         this.context = context;
         this.layout = layout;
         this.arrayList = arrayList;
+        arrayFilter= arrayList;
     }
 
     @Override
     public int getCount() {
-        return  arrayList.size();
+        return  arrayFilter.size();
     }
 
     @Override
@@ -49,9 +51,10 @@ public class FoodAdapters extends BaseAdapter {
         TextView tvRice= view.findViewById(R.id.rice);
         ImageView imgTv=  view.findViewById(R.id.img);
         //Gan gia tri
-        tvTitle.setText(arrayList.get(i).getTitle());
-        tvRice.setText(arrayList.get(i).getRice());
-        imgTv.setImageResource(arrayList.get(i).getImg());
+        Food food = arrayFilter.get(i);
+        tvTitle.setText(food.getTitle());
+        tvRice.setText(food.getRice());
+        imgTv.setImageResource(food.getImg());
 
     view.setOnClickListener(new View.OnClickListener() {
     @Override
@@ -69,5 +72,34 @@ public class FoodAdapters extends BaseAdapter {
     }
         return view;
 
+    }
+    public void setFilter(String filter) {
+        arrayFilter = new ArrayList<>();
+        System.out.println(filter);
+        for(Food food: arrayList) {
+            //indexOf: tim chuoi trong chuoi: thanh cong (123) => -1
+            if(food.getTitle().toLowerCase().indexOf(filter) != -1) {
+                arrayFilter.add(food);
+            }
+        }
+        System.out.println(arrayFilter);
+
+        notifyDataSetChanged();
+    }
+    public void setFilterBySearch(String giaTriInPut) {
+        //123
+        setFilter(giaTriInPut);
+    }
+
+    public void setAllDonut() {
+        arrayFilter = arrayList;
+        notifyDataSetChanged();
+    }
+
+    public void setFilterPink() {
+        setFilter("pink");
+    }
+    public void setFilterFloating() {
+        setFilter("floating");
     }
 }
